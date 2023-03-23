0342bcb8c678f80dbe27c21e2967f0d756f2ca3e
Thanh Dat <tranlethanhdat1995@gmail.com>
2023-03-23 19:00:08 +0900

    Config setting for deploy AWS

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,28 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.2"
 
-set :application, "freedoom"
-set :repo_url, "git@github.com:dtvip128/freedoom.git"
-set :ssh_options, { :forward_agent => true }
-
+set :application, 'freedoom'
+set :user, 'deploy'
+# Repository github của bạn. Tạo 1 repo mới trên github
+set :repo_url, 'git@github.com:dtvip128/freedoom.git'
 set :branch, :main
 set :deploy_to, '/home/deploy/freedoom'
 set :pty, true
-set :linked_files, %w(config/database.yml config/application.yml)
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :passenger_restart_with_touch, true
 set :rvm_ruby_version, 'ruby-2.7.7'
 
-set :puma_rackup, -> {File.join(current_path, "config.ru")}
-set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
-set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
-set :puma_bind, -> {"unix://#{shared_path}/tmp/sockets/puma.sock"}
-set :puma_conf, -> {"#{shared_path}/puma.rb"}
-set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
-set :puma_error_log, -> {"#{shared_path}/log/puma_error.log"}
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, "production"))
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
 set :puma_workers, 0
 set :puma_worker_timeout, nil
