f8a73996d481a2c1e99e8d128b543f51de412501
Thanh Dat <tranlethanhdat1995@gmail.com>
2023-03-24 15:47:51 +0700

    Crawl data from youtube

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,17 @@
+class VideosController < ApplicationController
+  def new
+    @video = Video.new
+  end
+
+  def create
+    CrawlService.new(current_user, video_params).execute
+
+    redirect_to root_url
+  end
+
+  private
+
+  def video_params
+    @video_params ||= params.require(:video).permit(:url)
+  end
+end
